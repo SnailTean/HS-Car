c3822dc726fd5b939dcd874618917d5abee5b393
unknown <zhangmm21303@hs.handsome.com.cn>
2017-07-13 19:50:09 +0800

    zmm-del

--- hscar/hscar-api/src/main/java/com/hundsun/hscar/vo/OrderVo.java
+++
@@ -1,5 +0,0 @@
-package com.hundsun.hscar.vo;
-
-public class OrderVo {
-	
-}
--- hscar/hscar-business/src/main/java/com/hundsun/hscar/service/api/IOrderService.java
+++
@@ -1,6 +0,0 @@
-package com.hundsun.hscar.service.api;
-
-
-public interface IOrderService {
-	public void sendOrder();
-}
