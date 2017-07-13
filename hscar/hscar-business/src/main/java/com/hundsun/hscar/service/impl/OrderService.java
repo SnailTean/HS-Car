750f773a6ab251c2c0a76f33c41f8006cea083ed
unknown <zhangmm21303@hs.handsome.com.cn>
2017-07-13 11:24:26 +0800

    整合

---
+++ hscar/hscar-business/src/main/java/com/hundsun/hscar/service/impl/OrderService.java
@@ -0,0 +1,16 @@
+package com.hundsun.hscar.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.hundsun.hscar.service.api.IOrderService;
+
+@Service("orderService")
+public class OrderService implements IOrderService {
+
+	@Override
+	public void sendOrder() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
