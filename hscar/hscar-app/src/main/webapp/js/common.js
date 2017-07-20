f7c6aaa0c8a42cee5a29b510b4032e3c9effc50a
unknown <zhangmm21303@hs.handsome.com.cn>
2017-07-20 17:25:45 +0800

    zmm

--- hscar/hscar-app/src/main/webapp/js/common.js
+++ hscar/hscar-app/src/main/webapp/js/common.js
@@ -30,7 +30,6 @@ $.ajaxSetup({
         "token": token
     },
     complete: function(xhr) {
-    	console.log(xhr);
         //token过期，则跳转到登录页面
         if(xhr.responseJSON.code == 401){
             parent.location.href = baseURL + 'login.html';
