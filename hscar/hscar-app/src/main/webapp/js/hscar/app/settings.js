105f56096ab7b25db294a87680807c532527c5c8
unknown <zhangmm21303@hs.handsome.com.cn>
2017-07-20 16:40:09 +0800

    app-司机主页

---
+++ hscar/hscar-app/src/main/webapp/js/hscar/app/settings.js
@@ -0,0 +1,17 @@
+$("#settings_close").click(function(){
+	alert("close");
+});
+
+$("#settings_test").click(function(){
+	alert('测试成功', function() {
+		
+	});
+});
+
+$("#settings_logout").click(function(){
+	//删除本地token
+    localStorage.removeItem("token");
+	
+    //跳转到登录页面
+    location.href = 'login.html';
+});
